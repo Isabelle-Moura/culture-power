3a291366835c5b7d0b070129f4d27d298d4be77f
Isabelle-Moura <mourabisabelle@gmail.com>
2024-01-24 17:23:42 -0300

    fix: gitignore file.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,4 +3,5 @@ import { userRouter } from "../modules/user/routes/user.routes";
 
 export const routes = Router();
 
+//TODO:Testa senhora, pra ver se tá bão.
 routes.use(userRouter); // Every user's routes.
